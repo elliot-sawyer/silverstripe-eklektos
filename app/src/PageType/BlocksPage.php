dd8a7ff62602f2529ba307bd571730fe205d950d
David Malone <davidm@catalyst.net.nz>
2019-01-17 10:49:35 +1300

    MAJOR: Fixed broken stuff by reverting elemental

---
+++ app/src/PageType/BlocksPage.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Eklektos\Eklektos\PageType;
+
+use Page;
+use Eklektos\Eklektos\PageType\LandingPage;
+use SilverStripe\Assets\Image;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+
+class BlocksPage extends Page
+{
+	/**
+	 * @var string
+	 * @config
+	 */
+	private static $table_name = 'BlocksPage';
+
+	/**
+	 * @var array
+	 * @config
+	 */
+	private static $db = array(
+		'MegamenuDescription' => 'Varchar(255)'
+	);
+
+	/**
+	 * @var string
+	 * @config
+	 */
+	private static $description = 'Generic page with blocks. Allows the use of content blocks.';
+
+	/**
+	 * @var array
+	 * @config
+	 */
+	private static $has_one = array(
+		'LandingPageSummaryImage' => Image::class
+	);
+
+	/**
+	 * @var array
+	 * @config
+	 */
+	private static $owns = array(
+		'LandingPageSummaryImage'
+	);
+
+	/**
+	 * @return FieldList
+	 */
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+		if ($this->Parent() instanceof LandingPage) {
+			$fields->addFieldsToTab('Root.Main', array(
+				TextAreaField::create('LandingPageSummary', 'Landing Page Summary'),
+				UploadField::create('LandingPageSummaryImage', 'Landing Page Summary Image')
+					->setDescription('Image size: 150 x 150')
+					->setAllowedFileCategories('image')
+					->setAllowedExtensions(array('jpg', 'jpeg', 'png', 'gif'))
+					->setFolderName('Landing Page Images')
+			));
+		}
+		return $fields;
+	}
+
+}
